c678bbe934628736ba42c27b1f99a606af3f3fc2
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-21 18:22:21 +0100

    Adding the site class

---
+++ src/Site.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package gypcms
+ */
+
+namespace gypcms;
+
+/**
+ * Class that contains functionality for generating classes from a wsdl file
+ *
+ * @package gypcms
+ * @author Fredrik Wallgren <fredrik@wallgren.me>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class Site
+{
+  /**
+  *
+  * @var string The name of the site
+  */
+  protected $name;
+}
