f4878924a55517ba971ca84fe31d8304393da2e3
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-22 01:18:55 +0100

    Adding the search result page

---
+++ src/page/SearchPage.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package gypcms
+ */
+
+namespace gypcms\page;
+
+/**
+ * The page used to display search results
+ *
+ * @package gypcms
+ * @author Fredrik Wallgren <fredrik@wallgren.me>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class SearchPage implements \gypcms\page\Page
+{
+  public function render()
+  {
+    
+  }
+}
