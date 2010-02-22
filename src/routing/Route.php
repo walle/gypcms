17a6bd2e63f7701ab5910d673b5a69cbbe97048e
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-22 01:10:15 +0100

    Adding the routing namespace

---
+++ src/routing/Route.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package gypcms
+ */
+
+namespace gypcms\routing;
+
+/**
+ * Represents a route
+ *
+ * @package gypcms
+ * @author Fredrik Wallgren <fredrik@wallgren.me>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class Route
+{
+  /**
+   *
+   * @var string The route
+   */
+  private $route;
+
+  /**
+   *
+   * @var string The destination
+   */
+  private $destination;
+}
