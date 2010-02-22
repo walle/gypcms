a6f4458f3555678ce530f938cc211da65164413c
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-22 02:13:55 +0100

    Added theme support and modified the testscript in index to fit

--- web/index.php
+++ web/index.php
@@ -7,27 +7,20 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-var_dump($_REQUEST);
-
 include_once dirname(__FILE__).'/../lib/yaml/lib/sfYaml.php';
 
 $settingsArr = sfYaml::load(dirname(__FILE__).'/../config/settings.yml');
 
 $first = sfYaml::load(dirname(__FILE__).'/../data/'.str_replace('.html', '.yml', $_REQUEST['url']).'');
 
-print '<pre>';
-var_dump($first);
-print '</pre>';
-
 include_once dirname(__FILE__).'/../lib/twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem(dirname(__FILE__).'/../templates/');
+$loader = new Twig_Loader_Filesystem(dirname(__FILE__).'/../templates/'.$settingsArr['settings']['theme']);
 
 $twig = new Twig_Environment($loader, array(
   'cache' => dirname(__FILE__).'/../cache/twigCache/',
 ));
 
-$template = $twig->loadTemplate('layout.html');
-
-$template->display($settingsArr['settings']);
+$template = $twig->loadTemplate('article.html');
+$template->display(array_merge(array_pop($settingsArr), array_pop($first)));
