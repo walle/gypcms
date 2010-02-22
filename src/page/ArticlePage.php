0372b0c043a4be1b55009175d2cd4d101c71f2e9
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-22 01:20:19 +0100

    Adding the article page

---
+++ src/page/ArticlePage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package gypcms
+ */
+
+namespace gypcms\page;
+
+/**
+ * The page used to display a article
+ *
+ * @package gypcms
+ * @author Fredrik Wallgren <fredrik@wallgren.me>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class ArticlePage implements \gypcms\page\Page
+{
+  public function render()
+  {
+    
+  }
+}
