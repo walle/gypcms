7c29ebb0e79e1ddf4f3d0a4a0945403b3dd11ca5
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-22 02:36:35 +0100

    Adding the rss page

---
+++ src/page/RssPage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package gypcms
+ */
+
+namespace gypcms\page;
+
+/**
+ * The page used to display lists of articles in rss format
+ *
+ * @package gypcms
+ * @author Fredrik Wallgren <fredrik@wallgren.me>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class RssPage implements \gypcms\page\Page
+{
+  public function render()
+  {
+    
+  }
+}
