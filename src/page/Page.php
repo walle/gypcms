1ddb08eeffaee6c1b64e46e9894047ffc0559d94
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-24 08:40:53 +0100

    Changed the interface to an abstract base class

--- src/page/Page.php
+++ src/page/Page.php
@@ -7,16 +7,75 @@
 namespace gypcms\page;
 
 /**
- * Inteface for all pages
+ * Baseclass for all pages
  *
  * @package gypcms
  * @author Fredrik Wallgren <fredrik@wallgren.me>
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-interface Page
+abstract class Page
 {
   /**
-   * All pages must have a render function to display itself
+   *
+   * @var string The name of the template the page uses
    */
-  public function render();
+  protected $templateName;
+
+  /**
+   *
+   * @var \Twig_Template
+   */
+  protected $template;
+
+  /**
+   *
+   * @var array An array with settings for the page
+   */
+  protected $settings;
+
+  /**
+   *
+   * @var array An array containing the data for the template
+   */
+  protected $data;
+
+  /**
+   *
+   * Loads the template from the environment
+   * Loads all settings and data
+   *
+   * @param string $templateName
+   */
+  public function __construct($templateName)
+  {
+    $this->templateName = $templateName;
+
+    $twig = \gypcms\Site::getTwigEnvironment();
+
+    $this->template = $twig->loadTemplate($this->templateName);
+
+    $this->data = array();
+    $this->settings = array();
+
+    $this->loadSettings();
+    $this->loadData();
+  }
+
+  /**
+   * Loads all the data for the page view
+   */
+  public abstract function loadData();
+
+  /**
+   * Loads all the settings for the page view
+   */
+  public abstract function loadSettings();
+
+  /**
+   * Render the page
+   */
+  public function render()
+  {
+    $this->template->display($this->data);
+  }
 }
