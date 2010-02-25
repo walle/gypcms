85bf0c4e8246ddd2aaf67b9feb176426e513a8b7
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-02-25 17:06:01 +0100

    Implemented the list page

--- src/page/ListPage.php
+++ src/page/ListPage.php
@@ -13,10 +13,39 @@ namespace gypcms\page;
  * @author Fredrik Wallgren <fredrik@wallgren.me>
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-class ListPage implements \gypcms\page\Page
+class ListPage extends Page
 {
-  public function render()
+  const TEMPLATENAME = 'list.html';
+
+  /**
+   *
+   * @var string The path to the datafile directory
+   */
+  private $dir;
+
+  public function  __construct(\gypcms\requestHandler\Request $request, \gypcms\routing\Route $route, $dir)
   {
-    
+    $this->dir = $dir;
+    parent::__construct(ListPage::TEMPLATENAME);
+  }
+
+  public function loadData()
+  {
+    $files = scandir($this->dir);
+    foreach ($files as $file)
+    {
+      if ($file != '.' && $file != '..')
+      {
+        $data = \sfYaml::load($this->dir.'/'.$file);
+        $this->data['articles'][] = array_pop($data);
+      }
+    }
+
+    $this->data['title'] = basename($this->dir);
+  }
+
+  public function loadSettings()
+  {
+
   }
 }
